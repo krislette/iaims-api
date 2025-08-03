0e0c107afe478a592bde56908ecc40c43e60b4e4
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-03 20:02:08 +0800

    Added login functionality on controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,37 @@ class AuthController extends Controller
                 'user' => $user
             ], 200);
         } catch (\Exception $exception) {
-            return response()->json(['error' => $exception->getMessage()]);
+            return response()->json(['error' => $exception->getMessage()], 403);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validated = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 403);
+        }
+
+        $credentials = ['email' => $request->email, 'password' => $request->password];
+
+        try {
+            if (!auth()->attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            $user = User::where('email', $request->email)->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'user' => $user
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 401);
         }
     }
 }
