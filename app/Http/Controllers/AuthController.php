a9a80d32a68420943050649414f98aedf75d2fd0
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-03 23:29:57 +0800

    Added password confirmation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,8 @@ class AuthController extends Controller
         $validated = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
+            'password_confirmation' => 'required|string|min:6|same:password',
         ]);
 
         if ($validated->fails()) {
