4e6ea8fe89ca61cc7f8861a7763ec7678ef00d71
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-17 15:02:54 +0800

    Added agency routes on api

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,28 @@
 <?php
 
+use App\Http\Controllers\AgencyController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+ * .------------.
+ * | API Routes |
+ * '------------'
+ */
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Agency routes
+Route::prefix('v1')->group(function () {
+    Route::apiResource('agencies', AgencyController::class);
 });
