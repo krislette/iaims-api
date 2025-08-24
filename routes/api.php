a3cfc8b814de0fdde30298e81675d405991b7c98
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-24 15:21:42 +0800

    Added auditor endpoints on API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AgencyController;
+use App\Http\Controllers\AuditorController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,7 +23,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Agency routes
+// V1 routes
 Route::prefix('v1')->group(function () {
     Route::apiResource('agencies', AgencyController::class);
+    Route::apiResource('auditors', AuditorController::class);
 });
