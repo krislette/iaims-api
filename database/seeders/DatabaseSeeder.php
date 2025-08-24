d0babc086a98554a5214741002c8dc96a2fc075d
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-24 16:31:38 +0800

    Added audit type seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             AuditCriteriaSeeder::class,
             InternalControlSeeder::class,
             DocumentTypeSeeder::class,
+            AuditTypeSeeder::class,
         ]);
     }
 }
