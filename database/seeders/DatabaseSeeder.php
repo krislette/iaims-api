ca2368b0637f4071398e9c8b46e098f599cd0edc
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-08-24 18:03:17 +0800

    Added user account seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             InternalControlSeeder::class,
             DocumentTypeSeeder::class,
             AuditTypeSeeder::class,
+            UserAccountSeeder::class,
         ]);
     }
 }
